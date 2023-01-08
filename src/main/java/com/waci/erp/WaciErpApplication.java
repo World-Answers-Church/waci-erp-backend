7f81ee9e3fb2805d7831bc87cc797a838cec9198
Raymond Mutebi <raymond.mutebi@gmail.com>
2023-01-09 00:45:38 +0300

    Initialise springboot

---
+++ src/main/java/com/waci/erp/WaciErpApplication.java
@@ -0,0 +1,13 @@
+package com.waci.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaciErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaciErpApplication.class, args);
+	}
+
+}
